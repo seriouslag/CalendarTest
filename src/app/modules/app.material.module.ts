8306155673befdc9ee002f4ef694d8f5827af4fb
seriouslag <Landon.Gavin@gmail.com>
2018-03-02 05:07:39 -0500

    removed unused imports/dependencies

--- src/app/modules/app.material.module.ts
+++ src/app/modules/app.material.module.ts
@@ -1,20 +1,10 @@
 
 import {
-  MatProgressSpinnerModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSliderModule,
-  MatSnackBarModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatButtonModule,
-  MatCardModule,
-  MatDialogModule,
-  MatIconModule,
-  MatInputModule,
-  MatMenuModule ,
-  MatOptionModule, MatDatepickerModule, MatAutocompleteModule
+  MatProgressSpinnerModule, MatRippleModule, MatSelectModule,
+  MatSliderModule, MatToolbarModule, MatTooltipModule,
+  MatButtonModule, MatCardModule, MatDialogModule,
+  MatIconModule, MatInputModule, MatOptionModule, MatDatepickerModule,
+  MatAutocompleteModule
 } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatMomentDateModule} from '@angular/material-moment-adapter';
@@ -27,16 +17,14 @@ import {NgModule} from '@angular/core';
 @NgModule({
   imports: [
     FlexLayoutModule, MatButtonModule, MatCardModule, MatDialogModule,
-    MatIconModule, MatInputModule, MatMenuModule, MatOptionModule, MatProgressSpinnerModule,
-    MatRippleModule, MatSelectModule, MatSliderModule, MatMomentDateModule ,
-    MatSnackBarModule, MatTabsModule, MatTooltipModule, MatToolbarModule,
+    MatIconModule, MatInputModule, MatOptionModule, MatProgressSpinnerModule,
+    MatRippleModule, MatSelectModule, MatSliderModule, MatMomentDateModule, MatTooltipModule, MatToolbarModule,
     BrowserAnimationsModule, MatDatepickerModule, MatAutocompleteModule
   ],
   exports: [
     FlexLayoutModule, MatButtonModule, MatCardModule, MatDialogModule,
-    MatIconModule, MatInputModule, MatMenuModule, MatOptionModule, MatProgressSpinnerModule,
-    MatRippleModule, MatSelectModule, MatSliderModule, MatMomentDateModule ,
-    MatSnackBarModule, MatTabsModule, MatTooltipModule, MatToolbarModule,
+    MatIconModule, MatInputModule, MatOptionModule, MatProgressSpinnerModule,
+    MatRippleModule, MatSelectModule, MatSliderModule, MatMomentDateModule, MatTooltipModule, MatToolbarModule,
     BrowserAnimationsModule, MatDatepickerModule, MatAutocompleteModule
   ]
 })
